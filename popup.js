54e3044885d8b640aa5177e9ceb0452223e44d20
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2017-12-31 19:16:12 -0500

    Upgraded the js to a class

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-/* Main js file to control popup */
+/* Securiffy */
 
 const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
@@ -7,76 +7,105 @@ const SPECIAL = '!@#$%^&*';
 
 const DEFAULT = [UPPERCASE, LOWERCASE, NUMBERS, SPECIAL];
 
-// Generate a random password based on reqs
-let randomString = (len, charSets) => {
-    let charSet = charSets.join('');
-    let randomString = '';
+/* 
+    Class to control the popup UI, password
+    generation, and storage/retrieval of options
+*/
+class PasswordGenerator {
 
-    for (var i = 0; i < len; i++) {
-        let randomPos = Math.floor(Math.random() * charSet.length);
-        randomString += charSet.substring(randomPos, randomPos+1);
+    constructor() {
+        this.bindEvents();
+        this.retrieveSavedOptions();
     }
+
+    // Commonly used elements
+    getOptionsForm() { return $('#options'); }
+    getPasswordSpan() { return $('#pwd'); }
+
+    // Set up event handlers
+    bindEvents() {
+        $('#generate-pwd').on('click', () => {
+           this.generatePassword();
+        });
     
-    return randomString;
-};
-
-// Copy generated password to clipboard
-let copyPassword = () => {
-    $('#pwd').select();
-
-    try {
-        let successful = document.execCommand('copy');
-        let msg = successful ? 'successful' : 'unsuccessful';
-        // Display message
-    } catch (err) {
-        // Display error message
-    }
-};
-
-// Menu item clicked, update UI
-let updateMenu = (target) => {
-    $('.menu .item, .content-tab').removeClass('active');
-    $(`[data-tab="${target.data('tab')}"]`).addClass('active');
-};
-
-// Save the password options
-let saveOptions = () => {
-    let options = $('#options').serializeArray();
-    chrome.storage.sync.set({options}, () => {
-        console.log({options});
-    });
-};
-
-// Load saved options into the form
-let loadOptions = (options) => {
-    for (var elem of options) {
-        $(`[type="checkbox"][name="${elem.name}"]`).prop('checked', true);
-        $(`[type="text"][name="${elem.name}"]`).val(elem.value);
+        $('#copy-pwd').on('click', () => {
+            this.copyPassword();
+        });
+    
+        $('.menu .item').on('click', (e) => {
+            this.updateMenu($(e.currentTarget));
+        });
+    
+        this.getOptionsForm().on('change', (e) => {
+            this.saveOptions();
+        });
     }
 
-    $('#options').removeClass('loading');
-};
+    // Get saved options from chrome storage, send
+    // to loadOptions to render results
+    retrieveSavedOptions() {
+        chrome.storage.sync.get('options', (result) => {
+            this.options = result.hasOwnProperty('options') ? result.options : []; 
+            this.loadOptions();
+        });
+    }
 
-// Set up events
+    // Load saved options into the form
+    loadOptions() {
+        for (var elem of this.options) {
+            $(`[type="checkbox"][name="${elem.name}"]`).prop('checked', true);
+            $(`[type="number"][name="${elem.name}"]`).val(elem.value);
+            $(`[type="text"][name="${elem.name}"]`).val(elem.value);
+        }
+        this.getOptionsForm().removeClass('loading');
+    }
+
+    // Save the password options to chrome storage
+    saveOptions() {
+        let options = this.getOptionsForm().serializeArray();
+        chrome.storage.sync.set({options}, () => {
+            console.log("Saved options successfully");
+        });
+    }
+
+    // Menu item clicked, update UI
+    updateMenu(target) {
+        $('.menu .item, .content-tab').removeClass('active');
+        $(`[data-tab="${target.data('tab')}"]`).addClass('active');
+    }
+
+    // Generate password and display in span
+    generatePassword() {
+        let password = this.randomString(16, DEFAULT);
+        this.getPasswordSpan().val(password);
+    }
+
+    // Copy generated password to clipboard
+    copyPassword() {
+        this.getPasswordSpan().select();
+
+        try {
+            let successful = document.execCommand('copy');
+            let msg = successful ? 'successful' : 'unsuccessful';
+            // Display message
+        } catch (err) {
+            // Display error message
+        }
+    }
+
+    // Generate a random password based on reqs
+    randomString(len, charSets) {
+        let charSet = charSets.join('');
+        let randomString = '';
+
+        for (var i = 0; i < len; i++) {
+            let randomPos = Math.floor(Math.random() * charSet.length);
+            randomString += charSet.substring(randomPos, randomPos+1);
+        }
+        return randomString;
+    }
+}
+
 $(document).ready(function() {
-    $('#generate-pwd').on('click', () => {
-        $('#pwd').val(randomString(16, DEFAULT));
-    });
-
-    $('#copy-pwd').on('click', () => {
-        copyPassword();
-    });
-
-    $('.menu .item').on('click', (e) => {
-        updateMenu($(e.currentTarget));
-    });
-
-    $('#options').on('change', (e) => {
-        saveOptions();
-    });
-
-    chrome.storage.sync.get('options', (result) => {
-        let options = result.hasOwnProperty('options') ? result.options : []; 
-        loadOptions(options);
-    });
+    new PasswordGenerator();
 });
