4ee042c6fa200d1bd143997560f9b9c5bfe7f424
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2018-01-01 14:33:34 -0500

    MVC for js

--- popup.js
+++ popup.js
@@ -5,8 +5,6 @@ const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
 const NUMBERS = '0123456789';
 const SPECIAL = '!@#$%^&*';
 
-const DEFAULT = [UPPERCASE, LOWERCASE, NUMBERS, SPECIAL];
-
 
 class PasswordGeneratorModel {
     constructor(controller) {
@@ -38,21 +36,35 @@ class PasswordGeneratorModel {
     }
 
     updateOptions(optionsList) {
+        // serializeArray removes keys with a False val,
+        // so we need to init things to False here to that
+        // they'll actually save in Chrome's storage. True
+        // vals will be set in the loop below.
+        let reset = {
+            lowercase: false,
+            uppercase: false,
+            numbers: false,
+            special: false,
+            exclude_similar: false,
+            mask: false,
+        };
+
         for (var opt of optionsList) {
-            this.options[opt.name] = opt.value;
+            reset[opt.name] = opt.value;
         }
+        this._setOptionsFromObject(reset);
+    }
+
+    _setOptionsFromObject(options) {
+        Object.keys(options).forEach((key) => {
+            this.options[key] = options[key];
+        });
         this._saveOptionsToStorage();
     }
 
     _saveOptionsToStorage() {
         chrome.storage.sync.set({options: this.options}, () => {
             console.log("Saved options successfully");
-        });
-    }
-
-    _setOptionsFromObject(options) {
-        Object.keys(options).forEach((key) => {
-            this.options[key] = options[key];
         });
     }
 
@@ -78,11 +90,10 @@ class PasswordGeneratorModel {
 
 }
 
+
 class PasswordGeneratorView {
     
-    constructor() {
-
-    }
+    constructor() {}
 
     // Commonly used elements
     getOptionsForm() { return $('#options'); }
@@ -91,8 +102,7 @@ class PasswordGeneratorView {
 
     renderOptions(options) {
         Object.keys(options).forEach((key) => {
-            console.log(key, options[key])
-            $(`[type="checkbox"][name="${key}"]`).prop('checked', options[key]=='on');
+            $(`[type="checkbox"][name="${key}"]`).prop('checked', options[key]);
             $(`[type="number"][name="${key}"]`).val(options[key]);
             $(`[type="text"][name="${key}"]`).val(options[key]);
         });
@@ -100,6 +110,29 @@ class PasswordGeneratorView {
         this.getOptionsForm().removeClass('loading');
     }
 
+    updateMenu(target) {
+        if (target.data('tab') === undefined) return;
+        $('.menu .item, .content-tab').removeClass('active');
+        $(`[data-tab="${target.data('tab')}"]`).addClass('active');
+    }
+
+    displayPassword(password) {
+        this.getPasswordText().val(password);
+        this.getPasswordContainer().show();
+    }
+
+    copyPassword() {
+        this.getPasswordText().select();
+
+        try {
+            let successful = document.execCommand('copy');
+            let msg = successful ? 'successful' : 'unsuccessful';
+            // Display message
+        } catch (err) {
+            // Display error message
+        }
+    }
+    
 }
 
 
@@ -108,16 +141,13 @@ class PasswordGeneratorController {
     constructor() {
         this.model = new PasswordGeneratorModel(this);
         this.view = new PasswordGeneratorView();
-
         this.bindEvents();
     }
-
 
     // Commonly used elements
     getOptionsForm() { return $('#options'); }
     getPasswordText() { return $('#pwd'); }
     getPasswordContainer() {return $('#pwd-container'); }
-
 
     // Set up event handlers
     bindEvents() {
@@ -142,50 +172,25 @@ class PasswordGeneratorController {
         this.view.renderOptions(this.model.options);
     }
 
-    // Load saved options into the form
-    loadOptions(result) {       
-        let options = this.model.options;
-        for (var opt of options) {
-            $(`[type="checkbox"][name="${options[opt].name}"]`).prop('checked', true);
-            $(`[type="number"][name="${options[opt].name}"]`).val(options[opt].value);
-            $(`[type="text"][name="${options[opt].name}"]`).val(options[opt].value);
-        }
-        this.getOptionsForm().removeClass('loading');
-    }
-
-    // Save the password options to chrome storage
     saveOptions() {
         let options = this.getOptionsForm().serializeArray();
         this.model.updateOptions(options);
     }
 
-    // Menu item clicked, update UI
     updateMenu(target) {
-        if (target.data('tab') === undefined) return;
-        $('.menu .item, .content-tab').removeClass('active');
-        $(`[data-tab="${target.data('tab')}"]`).addClass('active');
+        this.view.updateMenu(target);
     }
 
-    // Generate password and display in span
     generatePassword() {
         let password = this.model.generatePassword();
-        this.getPasswordText().val(password);
-        this.getPasswordContainer().show();
+        this.view.displayPassword(password);
     }
 
-    // Copy generated password to clipboard
     copyPassword() {
-        this.getPasswordText().select();
-
-        try {
-            let successful = document.execCommand('copy');
-            let msg = successful ? 'successful' : 'unsuccessful';
-            // Display message
-        } catch (err) {
-            // Display error message
-        }
+        this.view.copyPassword();
     }
 }
+
 
 $(document).ready(function() {
     new PasswordGeneratorController();
